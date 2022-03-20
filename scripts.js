e83941fc0f654509c6f445613acfec6842cb418b
Gabriel Pozato <99850728+GPozato@users.noreply.github.com>
2022-03-20 09:25:25 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,16 @@
+var nome = "Gabriel"
+var notaDoPrimeiroBimestre = 2
+var notaDoSegundoBimestre = 2
+var notaDoTerceitoBimestre = 2
+var notaDoQuartoBimestre = 3
+
+var notaFinal= (notaDoPrimeiroBimestre + notaDoSegundoBimestre + notaDoTerceitoBimestre + notaDoQuartoBimestre) / 4
+
+
+
+console.log("Bem vindo, "+ nome)
+
+if(notaFinal >6) console.log("Parabens vc foi Aprovado "+nome)
+else console.log("Reprovado")
+
+console.log(notaFinal)
